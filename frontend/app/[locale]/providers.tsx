eeaff88f45065e2d72b527959e2b26dc5fb00a36
bbkenny <jouleself@gmail.com>
2026-08-29 22:29:41 +0100

    feat(frontend): wire PartnerAuthProvider and PriceProvider into providers

--- frontend/app/[locale]/providers.tsx
+++ frontend/app/[locale]/providers.tsx
@@ -9,6 +9,8 @@ import { CurrencyProvider } from "@/contexts/CurrencyContext";
 import { NetworkProvider } from "@/contexts/NetworkContext";
 import { VaultProvider } from "@/contexts/VaultContext";
 import { NotificationProvider } from "@/contexts/NotificationContext";
+import { PartnerAuthProvider } from "@/contexts/PartnerAuthContext";
+import { PriceProvider } from "@/contexts/PriceContext";
 
 export function Providers({ children, nonce }: { children: ReactNode; nonce?: string }) {
   return (
@@ -16,11 +18,15 @@ export function Providers({ children, nonce }: { children: ReactNode; nonce?: st
       <FreighterProvider>
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem nonce={nonce}>
           <CurrencyProvider>
-            <VaultProvider>
-              <SessionProvider>
-                <NotificationProvider>{children}</NotificationProvider>
-              </SessionProvider>
-            </VaultProvider>
+            <PriceProvider>
+              <VaultProvider>
+                <SessionProvider>
+                  <PartnerAuthProvider>
+                    <NotificationProvider>{children}</NotificationProvider>
+                  </PartnerAuthProvider>
+                </SessionProvider>
+              </VaultProvider>
+            </PriceProvider>
           </CurrencyProvider>
           <Toaster richColors closeButton position="bottom-right" />
         </ThemeProvider>
